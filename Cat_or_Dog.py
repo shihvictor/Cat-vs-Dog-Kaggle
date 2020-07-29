56ba116065cc47b9fc61a1608939a779a0a2e898
Victor Shih <shih.victor@outlook.com>
2020-07-28 23:03:09 -0700

    FINALLY changed to generators for input data.

--- Cat_or_Dog.py
+++ Cat_or_Dog.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
-# from PIL import Image
+from PIL import Image
 from skimage.transform import resize
-# from keras import layers
+from keras import layers
 from keras.layers import Input, ZeroPadding2D, Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
 from keras.models import Model
 from keras.callbacks import CSVLogger
@@ -14,39 +14,49 @@ import tensorflow as tf
 from tensorflow import keras
 import pickle
 from pathlib import Path
-from model20 import model20
 from keras.preprocessing.image import ImageDataGenerator
 
 
-
 """ CHANGE THESE TO SWITCH BETWEEN TRAINING AND LOADING """
-NEW_MODEL = True       # False if loading saved model. True if creating new model.
-MODEL_NAME = 'testmodel9_128_using_generator'
+NEW_MODEL = True      # False if loading saved model. True if creating new model.
+MODEL_NAME = 'model_9_testrun2'
 
 """ HYPERPARAMETERS """
 IMG_SIZE = 128
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 15
 
 """ FILE ..."""
 TRAIN_DIR = 'datasets/train'
+VALIDATION_DIR = 'datasets/validation'
 TEST_DIR = 'datasets/test'
 # print(os.listdir(TRAIN_DIR))
 
+""" CHANGE THIS TO USE VALIDATION DATASET """
 def create_data_generators():
-    datagen = ImageDataGenerator(validation_split=.2)
-    train_generator = datagen.flow_from_directory(directory=TRAIN_DIR, target_size=(IMG_SIZE, IMG_SIZE), class_mode='categorical', batch_size=BATCH_SIZE, shuffle=True, subset='training')
-    validation_generator = datagen.flow_from_directory(directory=TRAIN_DIR, target_size=(IMG_SIZE, IMG_SIZE), class_mode='categorical', batch_size=BATCH_SIZE, shuffle=True, subset='validation')
-    # add test generator
-    batchX, batchY = validation_generator.next()
-    print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
-    for i in range(3):
+    """
+    Generates training, validation, and prediction generators to load entire datasets without exceeding RAM.
+    :return: A tuple (train_generator, validation_generator, predict_generator), where the train and validation
+    generators yield batches of image data and labels and predict generator yields images.
+    """
+    train_image_datagen = ImageDataGenerator(rescale=1./255)
+    validation_image_datagen = ImageDataGenerator(rescale=1./255)
+    predict_image_datagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_image_datagen.flow_from_directory(directory=TRAIN_DIR, target_size=(IMG_SIZE, IMG_SIZE), class_mode='categorical', batch_size=BATCH_SIZE, shuffle=True)
+    validation_generator = validation_image_datagen.flow_from_directory(directory=VALIDATION_DIR, target_size=(IMG_SIZE, IMG_SIZE), class_mode='categorical', batch_size=BATCH_SIZE, shuffle=False)
+    predict_generator = predict_image_datagen.flow_from_directory(directory=TEST_DIR, target_size=(IMG_SIZE, IMG_SIZE), class_mode=None, batch_size=1, shuffle=False)
+    # debugging generators
+    # batchX, batchY = train_generator.next()
+    batchX, batchY = next(train_generator)
+    print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX[0].shape, batchX[0].min(), batchX[0].max()))
+    for i in range(5):
         img = batchX[i,:,:,:]
-        plt.imshow(img/255.)
+        plt.imshow(img)
         label = batchY[i,:]
         plt.title('label:'+str(label))
         plt.show()
-    return train_generator, validation_generator
+        batchX, batchY = train_generator.next()
+    return train_generator, validation_generator, predict_generator
 
 
 def label_img(img_name):
@@ -60,12 +70,6 @@ def label_img(img_name):
     if word_label == 'cat': return [1,0]
     #                             [no cat, very doggo]
     elif word_label == 'dog': return [0,1]
-
-
-# def unison_shuffled_copies(a, b):
-#     assert len(a) == len(b)
-#     p = np.random.permutation(len(a))
-#     return a[p], b[p]
 
 
 def create_train_data():
@@ -195,20 +199,20 @@ def plot_Acc_And_Loss(history_dict, save=True):
 
 # Generates batches from datasets. (Saving the entire train set as (20k, 128, 128, 3) is too large to store in memory.
 # (64, 64, 3) imgs can fit.)
-train_generator, validation_generator = create_data_generators()
-# validation_dataset = tf.data.Dataset.from_generator(lambda:validation_generator, (tf.float32, tf.float32))
+train_generator, validation_generator, predict_generator = create_data_generators()
+
 if NEW_MODEL:
     """Compile Model"""
     cat_dog_model = model((IMG_SIZE, IMG_SIZE, 3))
-    cat_dog_model.summary()
     opt = Adam(learning_rate=.0001)  # Set optimizer
     cat_dog_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])  # Compile model
+    cat_dog_model.summary()
 
     """Train the model"""
     Path('model_logs/'+MODEL_NAME+'_logs/').mkdir(parents=True)
     csv_logger = CSVLogger(filename='model_logs/'+MODEL_NAME+'_logs/'+MODEL_NAME+'_log.csv', separator=',', append=True)
-    model_history = cat_dog_model.fit_generator(generator=train_generator, steps_per_epoch=train_generator.samples//BATCH_SIZE, epochs=EPOCHS, validation_data=validation_generator, validation_steps=validation_generator.samples//BATCH_SIZE, shuffle=True, callbacks=[csv_logger])
-    # model_history = cat_dog_model.fit(x=train_generator, epochs=EPOCHS, callbacks=[csv_logger], validation_data=validation_dataset, shuffle=True)
+    # model_history = cat_dog_model.fit_generator(generator=train_generator, steps_per_epoch=train_generator.samples//BATCH_SIZE, epochs=EPOCHS, validation_data=validation_generator, validation_steps=validation_generator.samples//BATCH_SIZE, shuffle=True, callbacks=[csv_logger])
+    model_history = cat_dog_model.fit(x=train_generator, epochs=EPOCHS, steps_per_epoch=20000//BATCH_SIZE, callbacks=[csv_logger], validation_data=validation_generator, validation_steps=5000//BATCH_SIZE)
     # === save the model ===
     cat_dog_model.save(filepath='model/' + MODEL_NAME, overwrite=True)
 
@@ -220,91 +224,33 @@ if NEW_MODEL:
     with open('model_logs/'+MODEL_NAME+'_logs/'+MODEL_NAME+'_summary', 'w') as fh:
         # Pass the file handle in as a lambda function to make it callable
         cat_dog_model.summary(print_fn=lambda x: fh.write(x + '\n'))
-""" Old loading and partitioning.
-# LOAD DATA
-# === Either have (1) or (2) commented.
-# X_data, Y_data = create_train_data()  # (1)
-# process_test_data()
-# === If train data .npy file already created, load it instead.
-X_data = np.load('datasets/X_train_data.npy')   #(2)
-Y_data = np.load('datasets/Y_train_data.npy')   #(2)
-X_test_data = np.load('datasets/X_test_data.npy')
+else:
+    """Load the model"""
+    cat_dog_model = keras.models.load_model('model/'+MODEL_NAME)
+    # Load model history and plot loss and acc
+    with open('model/'+MODEL_NAME+'/trainHistoryDict', 'rb') as file_name:
+        model_history = pickle.load(file_name)
+    plot_Acc_And_Loss(model_history, save=False)    # Plot but don't save.
 
-for i in range(10):
-    plt.imshow(X_test_data[i])
-    plt.show()
-
-
-# PARTITION DATA
-M = X_data.shape[0]
-X_train = X_data[0:int(M*.8), :]    # Train and cv data
-Y_train = Y_data[0:int(M*.8), :]
-X_val = X_data[int(M*.8):, :]
-Y_val = Y_data[int(M*.8):, :]
-
-print("number of training examples : " + str(X_train.shape[0]))
-print("number of val examples : " + str(X_val.shape[0]))
-print("X_train shape : " + str(X_train.shape))
-print("Y_train shape : " + str(Y_train.shape))
-print("X_val shape : " + str(X_val.shape))
-print("Y_val shape : " + str(Y_val.shape))
-print("X_test_data shape : " + str(X_test_data.shape))
-"""
-
-# # Compile, Train, Save, Plot
-# if NEW_MODEL:
-#     """Compile Model"""
-#     # cat_dog_model = model(X_train.shape[1:])
-#     cat_dog_model = model20(X_train.shape[1:])      # Create model
-#     cat_dog_model.summary()     # Print summary
-#     opt = Adam(learning_rate=.0001)     # Set optimizer
-#     cat_dog_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])     # Compile model
+    """Evaluate model"""
+    print('\n@> Evaluating model')
+    # results = cat_dog_model.evaluate_generator(generator=validation_generator, steps=validation_generator.samples, verbose=1)
+    results = cat_dog_model.evaluate(x=validation_dataset, batch_size=32, verbose=1)
+    print ("Loss = " + str(results[0]))
+    print ("Test Accuracy = " + str(results[1]))
 #
-#     """Train the model"""
-#     Path('model_logs/'+MODEL_NAME+'_logs/').mkdir(parents=True)
-#     csv_logger = CSVLogger(filename='model_logs/'+MODEL_NAME+'_logs/'+MODEL_NAME+'_log.csv', separator=',', append=True)
-#     model_history = cat_dog_model.fit(x=X_train, y=Y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_val, Y_val), shuffle=True, callbacks=[csv_logger])
-#     #=== save the model ===
-#     cat_dog_model.save(filepath='model/'+MODEL_NAME, overwrite=True)
-#
-#     """Save model history and plot loss and acc"""
-#     with open('model/'+MODEL_NAME+'/trainHistoryDict', 'wb') as file_name:
-#         pickle.dump(model_history.history, file_name)       # Save history dict
-#     plot_Acc_And_Loss(model_history.history)        # Plot acc and loss over epochs
-#
-#     with open('model_logs/'+MODEL_NAME+'_logs/'+MODEL_NAME+'_summary', 'w') as fh:
-#         # Pass the file handle in as a lambda function to make it callable
-#         cat_dog_model.summary(print_fn=lambda x: fh.write(x + '\n'))
-# else:
-#     """Load the model"""
-#     cat_dog_model = keras.models.load_model('model/'+MODEL_NAME)
-#
-#     """Load model history and plot loss and acc"""
-#     with open('model/'+MODEL_NAME+'/trainHistoryDict', 'rb') as file_name:
-#         model_history = pickle.load(file_name)
-#     plot_Acc_And_Loss(model_history, save=False)    # Plot but don't save.
-#
-#     # print('\n@> Evaluating model')
-#     # results = cat_dog_model.evaluate(X_val, Y_val, batch_size=32, verbose=1)
-#     # print ("Loss = " + str(results[0]))
-#     # print ("Test Accuracy = " + str(results[1]))
-#
+#     """Generate Predictions"""
 #     print('\n@> Predicting Test Data')
-#     NUM_OF_PRED = 10
-#     pred = cat_dog_model.predict(X_test_data)
-#
-#     for i in range(NUM_OF_PRED):
-#         x = X_test_data[i]
-#         p = pred[i]
-#         plt.imshow(x)
-#         if p[0] > p[1]:
-#             plt.title("{:.4%} cat | {:.4%} dog -> CAT".format(p[0], p[1]))
-#         else:
-#             plt.title("{:.4%} cat | {:.4%} dog -> DOG".format(p[0], p[1]))
-#         plt.show()
-#
-#     predictions=pd.DataFrame(data=pred[:,1], columns=['label'])
-#     print(predictions.head())
-#     predictions.index += 1
-#     predictions.index.name = 'id'
-#     predictions.to_csv('prediction.csv')
+#     NUM_OF_PRED = 5
+#     predict_generator.reset()
+#     t11 = predict_generator.samples
+#     pred = cat_dog_model.predict_generator(generator=predict_generator, steps=predict_generator.samples, verbose=1)
+
+    # filenames = [int(''.join(filter(str.isdigit, p))) for p in predict_generator.filenames]
+    # predictions = pd.DataFrame({"id": filenames, "label": pred[:, 1]})
+    # predictions.sort_values(by="id", inplace=True)
+    # predictions.reset_index(inplace=True)
+    # print(predictions.head())
+    # predictions.to_csv('unsorted_results.csv')
+
+print('DONE')
--- organize_dir.py
+++ organize_dir.py
@@ -1,9 +1,41 @@
 import os
 import shutil
+from random import shuffle
+import numpy as np
 
-for img_name in os.listdir('datasets/train'):
-    if img_name.endswith('.jpg'):
-        if 'cat' in img_name:
-            shutil.move('datasets/train/'+img_name, 'datasets/train/cat/'+img_name)     # move to cat dir
-        elif 'dog' in img_name:
-            shutil.move('datasets/train/'+img_name, 'datasets/train/dog/'+img_name)     # move to dog dir
+n = 0
+if n == 1:
+    for img_name in os.listdir('datasets/train'):
+        if img_name.endswith('.jpg'):
+            if 'cat' in img_name:
+                shutil.move('datasets/train/'+img_name, 'datasets/train/cat/'+img_name)     # move to cat dir
+            elif 'dog' in img_name:
+                shutil.move('datasets/train/'+img_name, 'datasets/train/dog/'+img_name)     # move to dog dir
+elif n == 2:
+    for img_name in os.listdir('datasets/train/cat'):
+        if img_name.endswith('.jpg'):
+            shutil.move('datasets/train/cat/'+img_name, 'datasets/train/'+img_name)
+    for img_name in os.listdir('datasets/train/dog'):
+        if img_name.endswith('.jpg'):
+            shutil.move('datasets/train/dog/'+img_name, 'datasets/train/'+img_name)
+
+def partition_train_data():
+    cat_names = [img_name for img_name in os.listdir('datasets/train/cat') if 'jpg' in img_name]
+    shuffle(cat_names)
+    i = 0 # Number of moved images
+    for cat_name in cat_names:
+        shutil.move('datasets/train/cat/'+cat_name, 'datasets/validation/cat/'+cat_name)
+        i += 1
+        if i == 2500: break
+
+    dog_names = [img_name for img_name in os.listdir('datasets/train/dog') if 'jpg' in img_name]
+    shuffle(dog_names)
+    i = 0 # Number of moved images
+    for dog_name in dog_names:
+        shutil.move('datasets/train/dog/'+dog_name, 'datasets/validation/dog/'+dog_name)
+        i += 1
+        if i == 2500: break
+
+    print("DONE PARTITIONING")
+
+partition_train_data()
